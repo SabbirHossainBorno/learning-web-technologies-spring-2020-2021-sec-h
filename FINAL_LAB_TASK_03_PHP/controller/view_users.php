39a2c9fa53437da1227020cbe6e7738df9681b5f
SabbirHossainBorno <sabbirhossainborno@gmail.com>
2021-03-23 16:10:50 +0600

    all pages

---
+++ FINAL_LAB_TASK_03_PHP/controller/view_users.php
@@ -0,0 +1,29 @@
+<center>
+	<table border="1" cellpadding="5" cellspacing="0">
+		<tr><td colspan="4" align="CENTER">Users</td></tr>
+		<tr>
+			<td>ID</td><td>NAME</td><td>Email</td><td>USER TYPE</td>
+		</tr>
+		<?php
+                
+                $conn = mysqli_connect('localhost', 'root', '', 'webtech');
+                $sql = "select * from log_reg ";
+                $result = mysqli_query($conn, $sql);
+                while($user=mysqli_fetch_array($result))
+                {
+                        echo "<tr>";
+                        
+                        echo "<td>"; echo $user["id"]; echo "</td>";
+                        echo "<td>"; echo $user["name"]; echo "</td>";
+                        echo "<td>"; echo $user["email"]; echo "</td>";
+                        echo "<td>"; echo $user["type"]; echo "</td>";
+                        
+                        echo "</tr>"; }?>
+		
+		<tr>
+			<td colspan="3" align="right">
+				<a href="home.html">Go Home</a>
+			</td>
+		</tr>
+	</table>			
+</center>
